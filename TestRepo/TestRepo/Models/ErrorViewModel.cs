a305d1e8b9808a1ee404a8462ad2229b09cfde29
nikolastefanov <nikolay_stefanov1965@abv.bg>
2021-07-10 14:25:03 +0300

    createWebApp

---
+++ TestRepo/TestRepo/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TestRepo.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
